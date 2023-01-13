23a47215341c065f55c0ecaacde87c64109f6ce5
Mr. Quiet <mr.quiet.own@gmail.com>
2023-01-14 01:07:10 +0200

    update

--- js/app.js
+++ js/app.js
@@ -22,15 +22,56 @@
             }));
         }
     }), 0);
+    function AJAX_AJAX(url, {method = "GET", body, answers}) {
+        function XHR() {
+            return new Promise(((resolve, reject) => {
+                let xhr;
+                let form = new FormData;
+                if (window.XMLHttpRequest) xhr = new XMLHttpRequest; else if (window.ActiveXObject) xhr = new ActiveXObject("Microsoft.XMLHTTP");
+                if ("PUT" === method.toLocaleUpperCase() || "PATCH" === method.toLocaleUpperCase() || "POST" === method.toLocaleUpperCase()) form.append("data", JSON.stringify(body));
+                xhr.responseType = "json";
+                xhr.open(method.toLocaleUpperCase(), url);
+                xhr.onreadystatechange = () => {
+                    answers({
+                        readyState: xhr.readyState,
+                        status: xhr.status,
+                        statusText: xhr.statusText
+                    });
+                    if (4 == xhr.readyState && xhr.status >= 200 && xhr.status <= 299) resolve(xhr.response); else if (4 == xhr.readyState && xhr.status >= 400) reject({
+                        errorResponse: xhr.response
+                    });
+                };
+                xhr.send(JSON.stringify(body));
+            }));
+        }
+        return XHR();
+    }
+    function splitSearch(search) {
+        const arrSeacrh = search.split(/[\?\=]/g);
+        return arrSeacrh.filter((search => search));
+    }
+    function createObjectSearch(search) {
+        const arrSeacrh = splitSearch(search), objectResult = {};
+        console.log(arrSeacrh);
+        for (let i = 0; i < arrSeacrh.length; i++) {
+            const item = arrSeacrh[i], prevItem = arrSeacrh[i - 1];
+            if (!(i % 2)) objectResult[item] = ""; else objectResult[prevItem] = item;
+        }
+        return objectResult;
+    }
+    function showResult({search}, AJAX) {
+        createObjectSearch(search);
+    }
     window.addEventListener("DOMContentLoaded", (function(e) {
-        document.addEventListener("click", (function(e) {
+        const spollersBody = document.querySelector(".information__container");
+        spollersBody.addEventListener("click", (function(e) {
             const target = e.target;
-            if (target.matches(".spollers__title")) {
-                const titleHeight = target.offsetHeight;
-                const contentHeight = target.nextElementSibling.offsetHeight;
-                target.parentElement.style.cssText = `height: ${titleHeight} + ${contentHeight}px`;
+            if (target.matches(".spollers__btn-ibg_info")) {
+                const details = target.parentElement.parentElement.parentElement;
+                if (!details.getAttribute("open")) details.setAttribute("open", true); else details.removeAttribute("open");
             }
         }));
+        showResult(location, AJAX_AJAX);
     }));
     window["FLS"] = true;
     isWebp();
